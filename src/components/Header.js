4d7745f3d9557b6d63f07374ff1bf9be27c425ab
ziwang0411 <ziwang0411@gmail.com>
2021-02-22 22:51:39 -0800

    added particles, centered avatar logo and typed info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,52 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Avatar, Grid, Box } from "@material-ui/core";
 import avatar from "../avatar.png";
 import Typed from "react-typed";
+
+//CSS STYLES
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "tomato",
+  },
+  subtitle: {
+    color: "tan",
+    marginBottom: "3rem",
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
 const Header = () => {
+  const classes = useStyles();
   return (
-    <Box>
-      <Avatar src={avatar} alt="avatar" />
-      <Typography variant="h4">
-        <Typed strings={[Welcome]} />
+    <Box className={classes.typedContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={avatar} alt="avatar" />
+      </Grid>
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Welcome"]} typeSpeed={40} />
+      </Typography>
+      <br />
+      <Typography variant="h4" className={classes.subTitle}>
+        <Typed
+          strings={["Web Design", "Web Development", "Web Application"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
       </Typography>
     </Box>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,84 @@ import React from "react";
 import Navbar from "./Navbar";
 import PersistentNavBar from "./PersistentNavbar";
 import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+import { Sync } from "@material-ui/icons";
 
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute",
+    color: "red",
+  },
+});
 const Home = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <React.Fragment>
       <Navbar />
       <Header />
-    </div>
+      <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            color: {
+              value: "random",
+            },
+            links: {
+              color: {
+                value: "random",
+              },
+              enable: true,
+              opacity: 0.4,
+              width: 2,
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "tomato",
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 10,
+                size_min: 0.1,
+                Sync: true,
+              },
+            },
+          },
+          opacity: {
+            value: 0.7,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.1,
+              Sync: true,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+        }}
+      />
+    </React.Fragment>
   );
 };
 
